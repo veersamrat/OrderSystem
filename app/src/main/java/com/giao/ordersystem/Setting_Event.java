23a7444feefa48174108253e3151ce0bf04ecef4
vulonggiao0207 <vulonggiao0207@yahoo.com>
2016-02-25 14:49:21 +1100

    Setting function (cont) fixing bug and optimizing

--- app/src/main/java/com/giao/ordersystem/Setting_Event.java
+++ app/src/main/java/com/giao/ordersystem/Setting_Event.java
@@ -3,6 +3,8 @@ package com.giao.ordersystem;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
