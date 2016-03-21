1a04cd128504ffd5c5aa92673160063495a0bbff
vulonggiao0207 <vulonggiao0207@yahoo.com>
2016-03-21 23:59:30 +1100

    work with menu (cont)

--- app/src/main/java/com/giao/ordersystem/Menu_Event.java
+++ app/src/main/java/com/giao/ordersystem/Menu_Event.java
@@ -30,7 +30,7 @@ public class Menu_Event {
             categoryList.add(categoryDAO.list().get(i).getCategoryName());
         }
         categoryDAO.close();
-        ArrayAdapter<String> categoryAdapter = new ArrayAdapter<String> (context,android.R.layout.simple_spinner_item,categoryList);
+        ArrayAdapter<String> categoryAdapter =  new ArrayAdapter(context,android.R.layout.simple_spinner_item,categoryList);
         categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         categorySpinner.setAdapter(categoryAdapter);
     }
