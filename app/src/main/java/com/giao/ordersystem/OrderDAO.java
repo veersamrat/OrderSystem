8b59900e264fc78c1a70ce9e4b9543d56f08c6e7
vulonggiao0207 <vulonggiao0207@yahoo.com>
2016-03-23 23:57:16 +1100

    work with table function

--- app/src/main/java/com/giao/ordersystem/OrderDAO.java
+++ app/src/main/java/com/giao/ordersystem/OrderDAO.java
@@ -1,7 +1,17 @@
 package com.giao.ordersystem;
-
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.SQLException;
+import java.util.List;
+import java.util.ArrayList;
 /**
  * Created by Long on 2/13/2016.
  */
 public class OrderDAO {
+    private Context context;
+    public OrderDAO(Context context) {
+        this.context = context;
+    }
 }
