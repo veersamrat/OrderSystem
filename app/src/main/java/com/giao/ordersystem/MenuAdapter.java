680c82718b8b4db6e18ddf06f656f2bd7bd3c698
vulonggiao0207 <vulonggiao0207@yahoo.com>
2016-05-03 23:20:26 +1000

    Work with MenuAdapter
    The spinner display blank test items, but selected item still have
    correct value

--- app/src/main/java/com/giao/ordersystem/MenuAdapter.java
+++ app/src/main/java/com/giao/ordersystem/MenuAdapter.java
@@ -11,19 +11,17 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class MenuAdapter extends BaseAdapter implements View.OnFocusChangeListener{
+public class MenuAdapter extends BaseAdapter{
     private Context context;
     private ArrayList<DishBO> data;
     private static LayoutInflater inflater = null;
-    private EditText mText;
-    private final int minDelta;           // threshold in ms
-    private long focusTime;                 // time of last touch
-    private View focusTarget;
+    private TextView mText;
+    private Button mButton;
     public MenuAdapter(Context context, ArrayList<DishBO> data) {
         // TODO Auto-generated constructor stub
-        minDelta = 300;           // threshold in ms
-        focusTime = 0;                 // time of last touch
+
         View focusTarget = null;
         this.context = context;
         this.data= new ArrayList<DishBO>(data);
@@ -50,67 +48,28 @@ public class MenuAdapter extends BaseAdapter implements View.OnFocusChangeListen
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
         // TODO Auto-generated method stub
-        final Holder holder= new Holder();
-        final long mTextLostFocusTimestamp = -1;
         View vi;
+        vi=convertView;
         if(convertView == null)
         {
             LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             vi = inflater.inflate(R.layout.listview_layout2, null, true);
         }
-        else {
+        mText=(TextView) vi.findViewById(R.id.EditCategoryEditText);
+        mButton = (Button) vi.findViewById(R.id.DeleteCategoryButton);
+        DishBO temp = (DishBO) data.get(position);
+        mText.setText(temp.getDishName());
+        mButton.setTag(temp);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int index = (Integer) v.getTag();
+                DishBO dish = data.get(index);
+                data.remove(dish);
+                notifyDataSetChanged();
 
-            vi = convertView;
-            holder.edittext = (EditText) vi.findViewById(R.id.EditCategoryEditText);
-            mText=(EditText) vi.findViewById(R.id.EditCategoryEditText);
-            holder.button = (Button) vi.findViewById(R.id.DeleteCategoryButton);
-            DishBO temp = (DishBO) data.get(position);
-        /*    holder.edittext.setText(temp.getCategoryName());
-            holder.button.setTag(temp.getCategoryName());
-            holder.button.setTag(position);*/
-            holder.button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    int index = (Integer) v.getTag();
-                    DishBO dish = data.get(index);
-                    data.remove(dish);
-                    notifyDataSetChanged();
-                }
-            });
-            holder.edittext.setOnFocusChangeListener(this);
-            vi.setTag(holder);
-        }
+            }});
         return vi;
     }
-    public class Holder{EditText edittext; Button button;}
 
-    //This Listener used to keep enable the edit mode of EditText
-    /*If focus was on a different view, then that view loses focus
-    2. the target gains focus
-    3. soft keyboard pops up.
-            4. this causes the target to lose focus
-    5. the code detects this situation and calls target.requestFocus()
-    6. the leftmost, topmost view gains focus, due to Android nonsense
-    7. the leftmost view loses focus, due to requestFocus being called
-    8. target finally gains focus*/
-    @Override
-    public void onFocusChange(View v, boolean hasFocus) {
-        long t = System.currentTimeMillis();
-        long delta = t - focusTime;
-        if (hasFocus) {     // gained focus
-            if (delta > minDelta) {
-                focusTime = t;
-                focusTarget = v;
-            }
-        }
-        else {              // lost focus
-            if (delta <= minDelta  &&  v == focusTarget) {
-                focusTarget.post(new Runnable() {   // reset focus to target
-                    public void run() {
-                        focusTarget.requestFocus();
-                    }
-                });
-            }
-        }
-    }
 }
