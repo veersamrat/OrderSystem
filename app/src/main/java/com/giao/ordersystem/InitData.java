fd482d7220d3dd05ee49773b1e77d36b0ff29698
vulonggiao0207 <vulonggiao0207@yahoo.com>
2016-05-31 23:50:22 +1000

    Create Initial data

--- app/src/main/java/com/giao/ordersystem/InitData.java
+++ app/src/main/java/com/giao/ordersystem/InitData.java
@@ -28,7 +28,7 @@ public class InitData extends Activity{
             tableDAO.close();
         }
         //Insert default Category
-        String []categoryArr={"Entrée","Soup","Noodle/Rice","Vegetable/Salad","Chicken","Pork","Beef","Prawn","Seafood","Red wine","White Wine",
+        String []categoryArr={"Entrée","Soup","Noodle/Rice","Salad/Vegetarian","Chicken/Duck","Pork","Beef","Prawn","Seafood","Red wine","White Wine",
                 "Beer/Soft Drink","Spirit/Cocktails","Coffee/Dessert","Banquet","Special Request"};
         for(int i =0;i<14;i++)
         {
@@ -69,22 +69,50 @@ public class InitData extends Activity{
             dishDAO.create(categoryName[i],dishName[i],dishPrice[i],dishDecription[i],"0");
             dishDAO.close();
         }
-        String []categoryName1={"Soup","Soup","Soup","Soup","Soup","Soup","Soup","Soup"};
-        String []dishName1={"Sweet & Sour Prawn Soup","Sweet & Sour Chicken Soup", "Chicken & Sweet Corn Soup","Crab & Asparagus Soup"
-                ,"Rice Noodle Soup – Hu Tieu My Tho","Combination Steamboat ","Seafood Steamboat ", "Vegetarian Steamboat"};
-        String []dishPrice1={"9.0","9.0","9.0","9.0","9.0","36.0","36.0","36.0"};
+        String []categoryName1={"Soup","Soup","Soup","Soup","Soup","Soup","Soup","Soup","Soup","Soup","Soup"};
+        String []dishName1={"Sweet and Sour Prawn Soup","Sweet and Sour Chicken Soup", "Sweet and Sour Tofu Soup","Chicken & Sweet Corn Soup","Crab & Asparagus Soup"
+                ,"Rice Noodle Soup – Hu Tieu My Tho","Combination Steamboat ","Seafood Steamboat ", "Vegetarian Steamboat",
+                "Flat rice noodle beef soup","Flat rice noodle chicken soup"};
+        String []dishPrice1={"9.0","9.0","8.5","9.0","9.0","9.0","36.0","36.0","36.0","14.0","14.0"};
         String []dishDecription1={"A specialty of Southern Vietnam. Soured with tamarind, fresh tomato, and pineapple; lifted by a wetland herb (unique to Vietnamese food), and finished with okra for colour and texture",
         "A specialty of Southern Vietnam. Soured with tamarind, fresh tomato, and pineapple; lifted by a wetland herb (unique to Vietnamese food), and finished with okra for colour and texture",
-        "Popular traditional soup",
+        "A specialty of Southern Vietnam. Soured with tamarind, fresh tomato, and pineapple; lifted by a wetland herb (unique to Vietnamese food), and finished with okra for colour and texture",
+        "Popular traditional soup with fresh crab and asparagus",
         "Succulent chunks of crab meat in a tasty asparagus soup",
         "From My Tho Province, this special rice noodle soup is topped with prawns, calamari, chicken and pork.",
         "One of the most popular dishes in Vietnamese dining. Cooked in a delicious broth at your table with fresh meats, seafood and vegetables. It is shared by four people as an entrée",
         "One of the most popular dishes in Vietnamese dining. Cooked in a delicious broth at your table with fresh seafood and vegetables. It is shared by four people as an entrée",
-        "One of the most popular dishes in Vietnamese dining. Cooked in a delicious broth at your table with fresh tofu and vegetables. It is shared by four people as an entrée"};
-        String []categoryName2={};
-        String []dishName2={};
-        String []dishPrice2={};
-        String []dishDecription2={};
+        "One of the most popular dishes in Vietnamese dining. Cooked in a delicious broth at your table with fresh tofu and vegetables. It is shared by four people as an entrée",
+        "Traditional beef soup with bean sprout and basil",
+        "Traditional chicken soup with bean sprout and basil"};
+        String []categoryName2={"Noodle/Rice","Noodle/Rice","Noodle/Rice","Noodle/Rice","Noodle/Rice","Noodle/Rice","Noodle/Rice","Noodle/Rice"};
+        String []dishName2={"Crispy Fried Egge Noodle","Soft Egg Noodle", "Rice Noodle","Fried Rice","Mimosa Rice","Garlic Rice (Small)","Garlic Rice (Large)","Steamed Rice"};
+        String []dishPrice2={"22.5","22.5","22.5","13.5","10.0","6.5","9.0","2.5"};
+        String []dishDecription2={"Crispy Fried Egg Noodles smothered with your choice of vegetables, meat and seafood, or vegetables and seafood, or vegetables only. Chilli optional.",
+        "Soft Egg Noodles braised with your choice of vegetables, meat and seafood, or vegetables and seafood, or vegetables only. Chilli optional.",
+        "Rice Noodles braised with your choice of vegetables, meat and seafood, or vegetables and seafood, or vegetables only. Chilli optional.",
+        "With prawns, pork sausage, egg and peas",
+        "Stir-fried rice with chicken, a touch of butter and pepper","",""};
+
+        String []categoryName3={"Salad/Vegetarian","Salad/Vegetarian","Salad/Vegetarian","Salad/Vegetarian","Salad/Vegetarian","Salad/Vegetarian","Salad/Vegetarian","Salad/Vegetarian","Salad/Vegetarian","Salad/Vegetarian"};
+        String []dishName3={"Duck Salad","Vegetarin Duck Salad","Beef Salad","Papaya Salad","Vegetarian Salad","Bonfire Totu",
+                "Tofu sautéed in your choice of tasty sauce (optional chilli)","Salt and Pepper Tofu","Asian Broccoli","Asian Broccoli with Garlic Sauce"};
+        String []dishPrice3={"12.0","12.0","15.0","13.80","13.5","26.0","17.9","18.5","12.6","13.5"};
+        String []dishDescription3={"Harry’s signature duck salad. Twice cooked with aromatic spices; dressed with lime juice, kaffir lime and special Vietnamese ingredients, served with lightly pickle salad in lettuce leaves, accompanied by prawn crackers. ",
+        "Harry’s signature duck salad, with tofu based substitute. Cooked with aromatic spices; dressed with lime juice, kaffir lime and special Vietnamese ingredients, served with lightly pickle salad in lettuce leaves, accompanied by prawn crackers. ",
+        "Harry’s new succulent sous vide cooked beef, with green apple and pickle salad, prawn crackers and fish sauce. Serves up to 4 people. ",
+        "Sliced prawns and pork, with lightly papaya salad, prawn crackers and fish sauce. Serves up to 4 ",
+        "Delicious tofu with lightly pickled salad, herbs, and served with crushed peanuts and soy sauce. ",
+
+        "Tofu, shitake mushroom and onion, cooked in a pot at the table and served with rice paper, lettuce, herbs, pickles and Harry’s hoisin sauce. ",
+        "Tomato Sauce, Lemongrass Tofu, Curry Sauce,Sate Sauce ,Mixed Vegetables,Salt & Pepper Tofu",
+        "Salt and pepper with lime sauce",
+        "Stir-fried with Harry’s special sauce.",
+                "Stir-fried with garlic sauce. "
+
+        };
+
+
     }
 
 }
